fe8d0d9d4a958121aaa98718d9cf49ce946aa971
Akuna Joshua <akunajoshua@gmail.com>
2024-04-18 15:29:09 +0100

    react-router-dom configured for index page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Post from './Post';
 
 function App() {
   return (
-    <div className='blog'>
-      <Header />
-      <Post />
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route index element={
+          <div className='blog'>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
+          </div>
+        } />
+      </Routes>
+      
+    </BrowserRouter>
+);
 }
 
 export default App;
