a3aa0edddd843a50d80f371aecee05fc578cacd8
Akuna Joshua <akunajoshua@gmail.com>
2024-04-22 01:05:41 +0100

    fetch request made to server via register function

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 export default function RegisterPage(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function register(e){
+        e.preventDefault();
+        const response = await fetch("http://localhost:8080/register", {
+            method: "POST",
+            body: JSON.stringify({email, password}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
     return(
         <div className="form">
-            <form className="register">
+            <form className="register" onSubmit={register}>
                 <h2>Register</h2>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
+                <input type="email" 
+                        placeholder="Email"
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}/>
+                <input type="password" 
+                        placeholder="Password"
+                        value={password}
+                        onChange={e=>setPassword(e.target.value)}/>
                 <input type="submit" value="Register"/>
             </form>
         </div>
