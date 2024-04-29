0732c4607b0afab22bf7218c675603237a915381
Akuna Joshua <akunajoshua@gmail.com>
2024-04-29 12:04:48 +0100

    http request sent to backend login endpoint from login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
 export default function LoginPage(){
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function login(event){
+        event.preventDefault()
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type': 'application/json'}
+        })
+        console.log(await response.json())
+    }
+
     return(
         <div className="form">
-            <form className="login">
+            <form className="login" onSubmit={login}>
                 <h2>Login</h2>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
+                <input type="text" 
+                       placeholder="Username"
+                       value={username}
+                       onChange={e=> setUsername(e.target.value)}/>
+                <input type="password"
+                       placeholder="Password"
+                       value={password}
+                       onChange={e=> setPassword(e.target.value)}/>
                 <input type="submit" value="Login"/>
             </form>
         </div>
