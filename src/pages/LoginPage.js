b0ae985bb27aca31693b18310a4ed0afdc17b2f7
Akuna Joshua <akunajoshua@gmail.com>
2024-05-05 00:30:52 +0100

    configured the login page to redirect to homepage on user login successful

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,17 +1,28 @@
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function LoginPage(){
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false)
 
     async function login(event){
         event.preventDefault()
         const response = await fetch('http://localhost:8080/login', {
             method: 'POST',
             body: JSON.stringify({username, password}),
-            headers: {'Content-Type': 'application/json'}
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
         })
-        console.log(await response.json())
+        if (response.ok){
+            setRedirect(true)
+        } else {
+            console.log('invalid username or password')
+        }
+    }
+
+    if (redirect){
+        return <Navigate to={'/'} />
     }
 
     return(
